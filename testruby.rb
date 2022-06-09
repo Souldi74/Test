81875bf8803c02d549f46c8a7f238b795252892e
Souldi74 <xaradis@yandex.ru>
2022-06-09 15:11:21 +0500

    commit 09.06

--- testruby.rb
+++ testruby.rb
@@ -1,6 +1,23 @@
 puts "введите ващу месячную зарплату :"
 pay_job = gets.to_f
-puts "Ваша зарплата в год #{ pay_job * 12} тысяч рублей"
-puts " Сумма отложеных средст за 6 лет равна : #{pay_job * 60}"
+yers_pay = pay_job * 12
+pr_pay = yers_pay / 15
+puts "Ваша зарплата в год #{yers_pay} тысяч рублей"
+puts " Сумма отложеных  средст за 6 лет равна : #{pay_job * 60}"
 gets
 
+x = 'Я молодец'
+x = x.downcase
+puts x
+a = 'HI'
+b = a
+a = 'xxx'
+b = a
+puts b
+
+sum = 0
+24.times do |n|
+
+ sum = sum + 500
+ puts "Месяц #{n}, у бабушки в сундуке #{sum}"
+end
