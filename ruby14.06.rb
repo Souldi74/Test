89d81f7a2c9a22fea8b664430c1470ba0d5e3391
Souldi74 <xaradis@yandex.ru>
2022-06-14 15:55:47 +0500

    14ruby

--- ruby14.06.rb
+++ ruby14.06.rb
@@ -6,3 +6,14 @@ answer = gets.chomp.downcase
 if age >= 18 && answer == 'y'
     puts 'Access dinated'
 end
+
+puts 'Введите ваш логин и пароль '
+puts 'Ваш логин: '
+log_in = gets.chomp.downcase
+puts 'Ваш пароль: '
+pass = gets.to_i
+if (log_in == 'admin' && pass == 12345)
+    puts 'Доступ разрешен'
+
+end
+
