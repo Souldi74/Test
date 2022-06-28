6240b84843197dccacdb2812aead7f9e98a3cff1
Souldi74 <xaradis@yandex.ru>
2022-06-28 16:42:43 +0500

    28april

--- 27april.rb
+++ 27april.rb
@@ -1,80 +1,103 @@
-@humans = 10_000
-@machines = 10_000
-
-
-
-
+@humans = 10
+@machines = 10
 
 def luck?
-  rand(0..1) == 1
+  rand(0..1)==1
 end
 
 def boom
-  diff = rand(1..5)
+  diff = rand (1..5)
   if luck?
-  @machines -= diff
-  puts "#{diff} машин уничтожено"
+    @machines -=diff
+    puts "#{diff} Машин уничтожено"
   else
-  @humans -= diff
-  puts "#{diff} людей погибло"
-  end
-end
-  # Метод возвращает случайное название города
-def random_city
-  dice = rand(1..5)
-  if dice == 1
-  'Москва'
-  elsif dice == 2
-  'Лос-Анджелес'
-  elsif dice == 3
-  'Пекин'
-  elsif dice == 4
-  'Лондон'
-  else
-  ' Сеул'
+      @humans -=diff
+
+      puts "#{diff} людей погибло"
+
+    end
   end
 
-  def random_sleep
-  sleep rand(0.3..1.5)
+  def random_city
+    dice = rand(1..5)
+    case dice
+    when 1
+      'Москва'
+    when 2
+      'Лондон'
+    when 3
+      'Будапешт'
+    when 4
+      'токио'
+    else
+    'Банкок'
+    end
   end
+  def random_sleep
+    sleep rand(0.2..1.5)
+  end
+
   def stats
     puts "Осталось #{@humans} людей и #{@machines} машин"
   end
+
   def event1
     puts "Запущена ракета по городу #{random_city}"
     random_sleep
     boom
   end
-
   def event2
-    puts "Применено радиоактивное оружие в городе #{random_city}"
+    puts" Применено радиактивное оружие в городе #{random_city}"
     random_sleep
     boom
   end
-
   def event3
-    puts "Группа солдат прорывает оборону противника в городе #{random_city}"
+    puts "группа солдат проывает оборону противника в городе #{random_city}"
+    random_sleep
+    boom
+  end
+  def event4
+    puts "Группа на единорогах атакует #{random_city}"
     random_sleep
     boom
   end
 
+  def event5
+    puts "киббер казаки атакуют #{random_city}"
+    random_sleep
+    boom
+  end
   def check_victory?
-    false
+      false
   end
-end
 
-loop do
-  if check_victory?
-  exit
-  end
-  dice = rand(1..3)
-  if dice == 1
-    event1
-  elsif dice == 2
-    event2
-  elsif dice == 3
-    event3
-  end
+  loop do
+    if check_victory?
+      exit
+    end
+    dice = rand (1..5)
+    case dice
+    when 1
+      event1
+    when 2
+      event2
+    when 4
+      event4
+    when 5
+      event5
+    else
+      event3
+    end
+    if @humans <=0
+      puts "Победа Машин #{@machines}"
+      exit
+    elsif @machines <=0
+      puts "Победа Людей #{@humans}"
+      exit
+    end
+
+
     stats
     random_sleep
-end
+  end
+
