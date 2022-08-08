9d65f37be891956058973fbdbd7541518ec68fef
Souldi74 <xaradis@yandex.ru>
2022-08-08 14:02:50 +0500

    8august

--- RubyRush/work9.rb
+++ RubyRush/work9.rb
@@ -3,10 +3,12 @@ foxy << 'Pinaple'
 foxy.push(2,'Aregano')
 foxy.bsearch{|x| x >=0}
 foxy.delete_at(-1)
+foxy.each{|x| print x, " Hello Kitty "}
 puts foxy.size
 #puts foxy.delete(5){" no found"}
 puts foxy.at(0)
 puts foxy.to_s
+puts foxy.empty?
 
 
 
